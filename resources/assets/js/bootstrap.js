35139032ae5b87db42887c3b177efc59087aa683
TolibRakhmonov <36322099+TolibRakhmonov@users.noreply.github.com>
2018-05-13 16:54:35 +0500

    Update bootstrap.js

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,6 +1,5 @@
 
 window._ = require('lodash');
-
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
  * for JavaScript based Bootstrap features such as modals and tabs. This
@@ -30,7 +29,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  */
 
 let token = document.head.querySelector('meta[name="csrf-token"]');
-
 if (token) {
     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
